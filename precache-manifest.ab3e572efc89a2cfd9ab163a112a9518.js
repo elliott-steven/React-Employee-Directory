82a98c377fb0acee1d540accf0aebb0e052d26e6
Steven Elliott <steven.elliott@live.com>
2021-05-27 12:47:55 -0400

    Updates

---
+++ precache-manifest.ab3e572efc89a2cfd9ab163a112a9518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f710b950750a3377a2597269a98b57",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "3e702e1d88ef72a568ce",
+    "url": "/React-Employee-Directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "92036a5983868eaf98d2",
+    "url": "/React-Employee-Directory/static/css/main.31ec2cc2.chunk.css"
+  },
+  {
+    "revision": "3e702e1d88ef72a568ce",
+    "url": "/React-Employee-Directory/static/js/2.da1954a1.chunk.js"
+  },
+  {
+    "revision": "92036a5983868eaf98d2",
+    "url": "/React-Employee-Directory/static/js/main.692204d1.chunk.js"
+  },
+  {
+    "revision": "cfc3c65f764df6d2ed22",
+    "url": "/React-Employee-Directory/static/js/runtime~main.c869fe2b.js"
+  }
+]);
